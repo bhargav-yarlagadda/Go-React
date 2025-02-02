73fa62350fa5430949705af92775853961123092
bhargav-yarlagadda <bhargavyarlagadda2003@gmail.com>
2025-02-02 15:10:41 +0530

    Client Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+  <div className='w-screen bg-black h-screen'>
+    <span className='text-white'>Hello </span>
+  </div>
+  )
+}
+
+export default App
